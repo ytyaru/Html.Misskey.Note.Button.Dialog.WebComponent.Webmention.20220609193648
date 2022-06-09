f3a3c3f0f9b37345a8a7ccee775f503d7964c960
ytyaru <yttry0@gmail.com>
2022-06-09 21:12:03 +0900

    toot,noteし終わったらコメント欄に表示する。

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,21 @@
 window.addEventListener('DOMContentLoaded', async (event) => {
     console.debug('DOMContentLoaded!!');
+    try {
+        window.mpurse.updateEmitter.removeAllListeners()
+          .on('stateChanged', isUnlocked => console.log(isUnlocked))
+          .on('addressChanged', address => console.log(address));
+    } catch(e) { console.error(e) }
+    document.querySelector(`toot-dialog`).addEventListener('toot', async(event) => {
+        console.debug('トゥートしました！webmentionしましたが受信リクエストせずともトゥートJSONから作成して表示します。', event.detail);
+        const html = new Comment().mastodonResToComment(event.detail.json)
+        const comment = document.querySelector(`mention-section`).shadowRoot.querySelector(`#web-mention-comment`)
+        comment.innerHTML = html + comment.innerHTML
+    });
     document.querySelector(`misskey-note-dialog`).addEventListener('note', async(event) => {
         console.debug('note event: ノートしました！', event.detail);
-        document.querySelector(`#res`).value = JSON.stringify(event.detail.json)
+        const html = new Comment().mastodonResToComment(event.detail.json.createdNote, event.detail.domain)
+        const comment = document.querySelector(`mention-section`).shadowRoot.querySelector(`#web-mention-comment`)
+        comment.innerHTML = html + comment.innerHTML
     });
 });
 window.addEventListener('beforeunload', (event) => {
